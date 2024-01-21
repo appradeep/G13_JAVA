ff3143218af3cee8940dd598240a1a9ea2a3e6f5
ASHOK PALIMIRI PRADEEP <85397898+appradeep@users.noreply.github.com>
2024-01-21 19:27:40 +0530

    Hybrid_Inheretance.java

--- Mini Projects/Pradeep/Assignment 2/Hybrid_Inheretance.java
+++ Mini Projects/Pradeep/Assignment 2/Hybrid_Inheretance.java
@@ -1,5 +1,7 @@
 package suretrust;
 
+package learningandpraticingjava;
+
 //super class
 class A {
    int a=1;
@@ -7,6 +9,9 @@ class A {
 //sub class
 class B extends A {
    int b=2;
+}
+class E extends A{
+	int e=5;
 }
 //class
 class C extends B{
@@ -19,14 +24,14 @@ class C extends B{
        System.out.println("The sum of a, b and c is " + sum);
    }
 }
-class D extends B {
-   int d = 4; 
-   int sum = a + b + d;
+class D extends E {
+   int d=4;
+   int sum = a + e + d;
    public void display() {
        System.out.println("The value of a is " + a);
-       System.out.println("The value of b is " + b);
+       System.out.println("The value of e is " + e);
        System.out.println("The value of d is " + d);
-       System.out.println("The sum of a, b and d is " + sum);
+       System.out.println("The sum of a, d and e is " + sum);
    }
 }
 
@@ -46,7 +51,6 @@ public class Hybrid2 {
 
 
 
-
 //OUTPUT:
 
 /*The value of a is 1
@@ -55,7 +59,7 @@ The value of c is 3
 The sum of a, b and c is 6
 
 The value of a is 1
-The value of b is 2
+The value of e is 5
 The value of d is 4
-The sum of a, b and d is 7
+The sum of a, d and e is 10
 */
